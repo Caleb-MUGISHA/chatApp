51cfb3a28c2ba94d02ac9757fefe502811abc4e9
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-05-27 09:10:47 +0200

    first prototype

--- web-messenger/src/App.js
+++ web-messenger/src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import homePage from "./containers/HomePage";
+import registerPage from "./containers/registerPage";
+import loginPage from "./containers/loginPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={homePage}></Route>
+        <Route path="/login" component={loginPage}></Route>
+        <Route path="/signup" component={registerPage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
